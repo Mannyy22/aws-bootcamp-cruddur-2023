c500cd994590b3f62b4c6ace079c2a7ff6fe41c4
Emmanuel Robertson <emman.soccer99@hotmail.com>
2023-03-19 18:55:27 +0000

    implemented jwt token to app

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -4,12 +4,14 @@ from opentelemetry import trace
 tracer = trace.get_tracer("home.activities")
 
 class HomeActivities:
-  def run():
+  def run(cognito_user_id=None):
     #logger.info("HomeActivities")
     with tracer.start_as_current_span("home-activities-mock-data"):
       span = trace.get_current_span()
       now = datetime.now(timezone.utc).astimezone()
       span.set_attribute("app.now", now.isoformat() )
+
+
       results = [{
         'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
         'handle':  'Andrew Brown',
@@ -49,5 +51,17 @@ class HomeActivities:
         'replies': []
       }
       ]
+      if cognito_user_id != None:
+        extra_crud = {
+        'uuid': 'Dude',
+        'handle':  'Hop',
+        'message': 'You wont guess what happened to me',
+        'created_at': (now - timedelta(hours=1)).isoformat(),
+        'expires_at': (now + timedelta(hours=12)).isoformat(),
+        'likes': 5,
+        'replies': []
+        }
+        results.insert(0, extra_crud)
+
       span.set_attribute("app.result_length", len(results))
       return results
